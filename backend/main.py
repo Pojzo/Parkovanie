04c5c67bd5702292c02deb4bdb1b60dffbbfe489
Peter Kovac <pojzinko8@gmail.com>
2024-11-01 11:07:58 +0100

    API description

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Parking API",
+    description="A simple API for a parking system",
+    version="0.1.0",
+    contact={
+        "Author 1": "Peter Kovac",
+        "Author 2": "Lukas Gulik"
+    }
+)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Ivka"}
+    return "Hello World"
