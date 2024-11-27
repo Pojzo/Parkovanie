2c96aaa344aae0c570c4310e5629cb22dd351589
Lukáš Grúlik <Lukas.Grulik@evpu.sk>
2024-11-27 09:52:43 +0100

    feat: Oprava chýb, úprava vzhľadu index.html, doplnenie odkazov na dokumenty, pridaný harmonogram, zmenená štruktúra.

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -8,3 +8,19 @@ function showWeek(weekId) {
         week.classList.add('active');
     }
 }
+
+const images = [
+    './res/images/parking1.jpg',
+    './res/images/parking2.jpg',
+    './res/images/parking3.jpg',
+];
+
+const bgImageDiv = document.querySelector('.bg-image');
+let currentImageIndex = 0;
+
+function changeBackgroundImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    bgImageDiv.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+}
+
+setInterval(changeBackgroundImage, 5000);
