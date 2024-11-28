64696c9e6725044bf6f74702ff17f1bfb1addf32
Lukáš Grúlik <Lukas.Grulik@evpu.sk>
2024-11-28 13:06:48 +0100

    feat: Aktualizácia vzhľadu reservation.html a admin.html, doplnenie chýbajúcich parameterov pre tvorbu garáže

--- frontend/scripts/api/createGarage.js
+++ frontend/scripts/api/createGarage.js
@@ -14,11 +14,15 @@ const _getGarageForm = () => {
     const name = document.getElementById("create-garage-name").value;
     const location = document.getElementById("create-garage-location").value;
     const floors = document.getElementById("create-garage-floors").value;
+    const num_rows = document.getElementById('create-garage-num-rows').value;
+    const num_cols = document.getElementById('create-garage-num-cols').value;
 
     return {
         name,
         location,
-        floors
+        floors,
+        num_rows,
+        num_cols
     }
 }
 
@@ -31,24 +35,26 @@ export const createGarage = async () => {
         alert('Form not found');
         return;
     }
-    const { name, location, floors } = formData;
+    const { name, location, floors, num_rows, num_cols } = formData;
 
     // Check if the form is filled out
-    if (!name || !location || !floors) {
+    if (!name || !location || !floors || !num_rows || !num_cols) {
         alert('Please fill out all fields');
+        return;
     }
+
     try {
         // Send the data to the server
         const response = await axios.post(garagesEndpoint, {
             name,
             location,
-            floors
+            floors,
+            num_rows,
+            num_cols
         });
         showToast('Garage created successfully', 'success');
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
-
         showToast(`Failed to create garage: ${error.response.data.message}`, 'danger');
     }
-}
+};
